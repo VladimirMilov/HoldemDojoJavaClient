440cb40fb4cbfe717e28e1160e9fc661dc189a75
Vladimir Milov <Vladimir_Milov@epam.com>
2017-10-21 00:19:28 +0300

    Added preflop logic

--- src/main/java/com/nedogeek/Client.java
+++ src/main/java/com/nedogeek/Client.java
@@ -7,16 +7,18 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URI;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Client {
-    private static final String userName = "someUser";
+    private static final String userName = "Vladi";
     private static final String password = "somePassword";
 
-    private static final String SERVER = "ws://77.47.200.184:8080/ws";
+    private static final String SERVER = "ws://10.22.40.124:8080/ws";
     private org.eclipse.jetty.websocket.WebSocket.Connection connection;
 
     enum Commands {
@@ -93,8 +95,8 @@ public class Client {
     String mover;
     List<String> event;
     List<Player> players;
-
     String cardCombination;
+
 
     public Client() {
             con();
@@ -136,7 +138,7 @@ public class Client {
     }
 
     private List<String> parseEvent(JSONArray eventJSON) {
-        List<String> events = new ArrayList<>();
+        List<String> events = new ArrayList<String>();
 
         for (int i = 0; i < eventJSON.length(); i++) {
             events.add(eventJSON.getString(i));
@@ -146,14 +148,14 @@ public class Client {
     }
 
     private List<Player> parsePlayers(JSONArray playersJSON) {
-        List<Player> players = new ArrayList<>();
+        List<Player> players = new ArrayList<Player>();
         for (int i = 0; i < playersJSON.length(); i++) {
             JSONObject playerJSON = (JSONObject) playersJSON.get(i);
             int balance = 0;
             int bet = 0;
             String status = "";
             String name = "";
-            List<Card> cards = new ArrayList<>();
+            List<Card> cards = new ArrayList<Card>();
 
             if (playerJSON.has("balance")) {
                 balance = playerJSON.getInt("balance");
@@ -178,7 +180,7 @@ public class Client {
     }
 
     private List<Card> parseCards(JSONArray cardsJSON) {
-        List<Card> cards = new ArrayList<>();
+        List<Card> cards = new ArrayList<Card>();
 
         for (int i = 0; i < cardsJSON.length(); i++) {
             String cardSuit = ((JSONObject) cardsJSON.get(i)).getString("cardSuit");
@@ -190,7 +192,197 @@ public class Client {
         return cards;
     }
 
+    int gameRounds = 0;
+    int bigBlind = 0;
+
     private void doAnswer() throws IOException {
-        connection.sendMessage(Commands.AllIn.toString());
+
+        System.out.println(myPlayer().name);
+        System.out.println(myPlayer().cards.get(0).suit + "  " + myPlayer().cards.get(0).value);
+        System.out.println(myPlayer().cards.get(1).suit + "  " + myPlayer().cards.get(1).value);
+
+//        gameRounds++;
+//        if(gameRounds <= 20) {
+//            System.out.println("GAME ROUNDS ARE: " + gameRounds);
+//            connection.sendMessage(Commands.Fold.toString());
+//            return;
+//        }
+
+        if(gameRound.equals("BLIND")) {
+            preFlopStrategy();
+        }
+
     }
+
+
+    private void preFlopStrategy() throws IOException {
+
+        String myCards = myPlayer().cards.get(0).value.toLowerCase().toString().concat(myPlayer().cards.get(1).value.toLowerCase().toString());
+
+        for (Player p : players) {
+            if (p.status.equals("BigBlind")) {
+                bigBlind = p.bet;
+            }
+        }
+        if(amIInGoodPosition()) {
+            System.out.println("VERY GOOD POSITION");
+        }
+        List<String> allInCards = new ArrayList<String>();
+        allInCards.add("aa");
+        allInCards.add("kk");
+        allInCards.add("qq");
+
+
+        for(int i = 0; i < allInCards.size(); i++) {
+            if(myCards.equals(allInCards.get(i)) || myCards.equals((new StringBuilder(allInCards.get(i)).reverse().toString()))) {
+                System.out.println("INTO ALL IN CARDS---------------");
+                connection.sendMessage(Commands.AllIn.toString());
+                return;
+            }
+        }
+
+        List<String> raiseCards = new ArrayList<String>();
+        raiseCards.add("ak");
+        raiseCards.add("aq");
+        raiseCards.add("aj");
+        raiseCards.add("at");
+        raiseCards.add("kq");
+        raiseCards.add("kj");
+        raiseCards.add("qj");
+        raiseCards.add("jt");
+        raiseCards.add("jt");
+        raiseCards.add("t9");
+
+        for(int i = 0; i < raiseCards.size(); i++) {
+            if(myCards.equals(raiseCards.get(i)) || myCards.equals((new StringBuilder(raiseCards.get(i)).reverse().toString()))) {
+                if (howMuchHasSomeoneRaised(bigBlind) >= myPlayer().balance / 3) {
+                    connection.sendMessage(Commands.Call.toString());
+                    return;
+                }
+                System.out.println("INTO RAISE CARDS---------------");
+                connection.sendMessage(Commands.Rise.toString() + "," + pot/2.5);
+                return;
+            }
+        }
+
+        List<String> callCards = new ArrayList<String>();
+        callCards.add("qt");
+        callCards.add("t8");
+        callCards.add("t7");
+        callCards.add("98");
+        callCards.add("97");
+        callCards.add("87");
+        callCards.add("76");
+        callCards.add("65");
+        callCards.add("54");
+        callCards.add("43");
+        callCards.add("32");
+        callCards.add("a2");
+        callCards.add("24");
+        callCards.add("35");
+        callCards.add("46");
+        callCards.add("57");
+        callCards.add("68");
+        callCards.add("79");
+        callCards.add("8t");
+
+        for(int i = 0; i < callCards.size(); i++) {
+            if(myCards.equals(callCards.get(i)) || myCards.equals((new StringBuilder(callCards.get(i)).reverse().toString()))) {
+                if (howMuchHasSomeoneRaised(bigBlind) >= myPlayer().balance / 3) {
+                    connection.sendMessage(Commands.Fold.toString());
+                    return;
+                }
+                System.out.println("INTO CALL CARDS---------------");
+                connection.sendMessage(Commands.Call.toString());
+                return;
+            }
+        }
+
+        if (howMuchHasSomeoneRaised(bigBlind) != 0) {
+            System.out.println("PREFLOP WILL CHECK---------------");
+            connection.sendMessage(Commands.Call.toString());
+            return;
+        } else {
+            System.out.println("PREFLOP WILL FOLD---------------");
+            connection.sendMessage(Commands.Fold.toString());
+            return;
+        }
+
+    }
+
+
+
+
+    private void flopStrategy() throws IOException {
+        if (howMuchHasSomeoneRaised(bigBlind) != 0) {
+            if (amIInGoodPosition()) {
+                connection.sendMessage(Commands.Rise.toString() + "," + bigBlind);
+                return;
+            }
+        }
+
+
+        List<String> allPossibleCardCombinations = new ArrayList<String>();
+        allPossibleCardCombinations.add("High card");
+        allPossibleCardCombinations.add("Pair of");
+        allPossibleCardCombinations.add("Two pairs");
+        allPossibleCardCombinations.add("Set of");
+        allPossibleCardCombinations.add("Straight");
+        allPossibleCardCombinations.add("Flash");
+        allPossibleCardCombinations.add("Full house");
+        allPossibleCardCombinations.add("Four of");
+
+        for(int i = 0; i < allPossibleCardCombinations.size(); i++) {
+
+        }
+
+
+    }
+
+
+
+
+//    private int positionOnTable() {
+//        for (int i = 0; i < players.size(); i++) {
+//            if (players.get(i).status.equalsIgnoreCase("smallblind")) {
+////                p = i;
+////            }
+////        }
+//            }
+//        }
+//    }
+
+    private int howMuchHasSomeoneRaised(int bigBlind){
+        int otherBetRaise = 0;
+        for (Player p : players) {
+            if (!p.name.equals(myPlayer().name)) {
+                if (p.bet > bigBlind) {
+                    otherBetRaise = p.bet;
+                }
+            }
+        }
+        return otherBetRaise;
+    }
+
+    // TODO fix this logic if you have time ->
+    private boolean amIInGoodPosition() {
+        if(dealer.equals(myPlayer().name)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    private Player myPlayer() {
+        Player myPlayer = null;
+        for(Player player : players) {
+            if(player.name.equals(userName)) {
+                myPlayer = player;
+                break;
+            }
+        }
+        return myPlayer;
+    }
+
 }
